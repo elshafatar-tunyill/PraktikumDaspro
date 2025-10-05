44d809f0642d55c49edff8b0f9a9060f6157ccf5
elshafatar-tunyill <elshafatar@gmail.com>
2025-10-05 11:46:06 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS08.java
+++ jobsheet5/ifCetakKRS08.java
@@ -5,8 +5,9 @@ public class ifCetakKRS08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.println("Pakah UKT Sudah Lunas? (true/false): ");
+        System.out.print("Pakah UKT Sudah Lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
+
 
         if (uktLunas) {
             System.out.println("Pembayaran UKT Terverifikasi");
@@ -14,6 +15,11 @@ public class ifCetakKRS08 {
         }else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
+
+        String pesan = (uktLunas) ? "Pembayaran UKT Terverifikasi,Silahkan Cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
+        
         sc.close();
+
     }   
 }
